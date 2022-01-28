62a27bb830338610656f32c2144b776d44ea2bce
mehmet yilmaz <mehmettatarstan@gmail.com>
2022-01-28 08:23:02 +0300

    docs:indexjs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = str.split("");
+  for (let i = 0; i < brackets.length; i++) {
+    for (let x = 0; x < bracketsConfig.length; x++) {
+      if (brackets[i] === bracketsConfig[x][0] && brackets[i + 1] === bracketsConfig[x][1]) {
+        brackets.splice(i, 2);
+        i = -1;
+        continue;
+      }
+    }
+  }
+  if (brackets.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
